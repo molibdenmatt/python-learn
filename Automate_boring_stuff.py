c07c5ebb6a2d3e7047c65c6f5175864add6ed317
Swieton Mateusz (Ext) <ext.mateusz.swieton@tieto.com>
2019-02-19 10:21:45 +0100

    Automate boring stuff - continues

--- Automate_boring_stuff.py
+++ Automate_boring_stuff.py
@@ -1,3 +1,59 @@
+# Automate Boring Stuff with Python - Udemy
+# https://www.udemy.com/automate/learn/v4/t/lecture/3662882?start=0
+# print("hello world")
+#
+# test = 'yo ' + 'man' + '!' * 5
+# print(test)
+#
+# print ('What is our name?')
+# myName = input()
+# print ('It is good to meet you, ' + myName)
+# print ('The length of your name is:')
+# print(len(myName))
+# print('What is your age?')
+# myAge = input()
+# print ('You will be ' + str(int(myAge)+1) + ' in a year.')
 
+# name = 'Pjoter'
+# if name == 'Mati':
+#     print('Hi Mati')
+# elif name == 'Janusz':
+#     print('Oj Janusz...')
+# else:
+#     print('Hi stranger')
+# print('Done')
+#
+# bool('')   # returns Falsy value
+#
+# name = ''
+# while name != 'your name':
+#     print("type your name")
+#     name = input()
+# print('Thanks')
 
-print("hello world")
+# name = ''
+# while True:
+#     print('type your name')
+#     name = input()
+#     if name == 'your name':
+#         break
+
+# spam = 0
+# while spam < 5:
+#     spam = spam + 1
+#     if spam == 3:
+#         continue #go back to while beginning
+#     print('spam is ' + str(spam))
+
+# print('My name is')
+# for i in range (5): #from i=0 to i=4 (not reaching 5)
+#     print ('Jimmy Five times ' + str(i))
+
+# total = 0
+# for num in range(101):
+#     total = total + num
+# print(total)
+
+for i in range(12, 32, 2): #(from, to, step)
+    print(i)
+
