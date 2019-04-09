c8d0226ad8206c269ad425d339b822a3ac488124
molibdentmatt <mateusz.swieton@gmail.com>
2019-04-09 12:31:36 +0200

    update

--- Automate_boring_stuff.py
+++ Automate_boring_stuff.py
@@ -1,11 +1,7 @@
 # Automate Boring Stuff with Python - Udemy
 # https://www.udemy.com/automate/learn/v4/t/lecture/3662882?start=0
-# print("hello world")
-#
 import random, sys, os, math
 import pyperclip
-
-
 
 # test = 'yo ' + 'man' + '!' * 5
 # print(test)
@@ -19,15 +15,6 @@ import pyperclip
 # myAge = input()
 # print ('You will be ' + str(int(myAge)+1) + ' in a year.')
 
-# name = 'Pjoter'
-# if name == 'Mati':
-#     print('Hi Mati')
-# elif name == 'Janusz':
-#     print('Oj Janusz...')
-# else:
-#     print('Hi stranger')
-# print('Done')
-#
 # bool('')   # returns Falsy value
 #
 # name = ''
@@ -50,6 +37,7 @@ import pyperclip
 #         continue #go back to while beginning
 #     print('spam is ' + str(spam))
 
+
 # total = 0
 # for num in range(101):
 #     total = total + num
@@ -62,24 +50,32 @@ import pyperclip
 # pyperclip.copy('Hello World copied')
 # print(pyperclip.paste())
 
-def hello(name):
-    print('Hello ' + name)
+# def hello(name):
+#     print('Hello ' + name)
+#
+#
+# hello('Alice')
+# hello('Bob ')
+#
+# s = input("Your name?") # easy prompt
+# print("Your name is: ", end="")
+# print(s)
+# print("Hello, {}".format(s)) #another method to print
+# x = 5/2.3
+# print("{:.2f}".format(x)) #:.2f - format float, two digits after coma
+#
+# for i in range(65,65+26):
+#     print("{} is {}".format(chr(i),i))
+
+for i in range(len(sys.argv)):
+    print(sys.argv[i]) #Will print every argument from command line e.g. python program.py arg1 arg2
 
 
-hello('Alice')
-hello('Bob ')
 
-s = input("Your name?") # easy prompt
-print("Your name is: ", end="")
-print(s)
-print("Hello, {}".format(s)) #another method to print
-x = 5/2.3
-print("{:.2f}".format(x)) #:.2f - format float, two digits after coma
 
-for i in range(65,65+26):
-    print("{} is {}".format(chr(i),i))
 
 
 
 sys.exit() # end the script
 print('You won\'t see me')
+
--- BB_black.py
+++ BB_black.py
@@ -1,6 +1,6 @@
 # Usage:
 # BB_black.py https://*/CXP9029710_27-f3f7c0d.cxp - Will unpack the .bin file
-# BB_black.py 10.64.175.61 - Will send the black to node and reboot it
+# BB_black.py 10.64.175.61 - Will send the black to node, setup and reboot it
 
 import subprocess,sys
 link = sys.argv[1]
@@ -20,19 +20,19 @@ if sys.argv[1][-4:] == ".cxp":
     term('mv ./squashfs-root/bbmContLteRdbfBlm4TrinityLm/bin/bbmContLteRdbfBlm4TrinityLmC.bin ./bbmContLteRdbfBlm4TrinityLmC.bin')
     #term('scp ./bbmContLteRdbfBlm4TrinityLmC.bin l:')
 else:
-HOST="root@"+sys.argv[1]
-ssh = subprocess.Popen(["ssh", "%s" % HOST],
-    shell=False,
-    stdin=subprocess.PIPE,
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-    universal_newlines=True,
-    bufsize=0)
-ssh.stdin.write("ln -s /rcs/bbmContLteRdbfBlm4TrinityLmC.bin /home/sirpa/dev_patches/bbmContLteRdbfBlm4TrinityLmC.bin \n")
-ssh.stdin.write("chmod 777  /home/sirpa/dev_patches/bbmContLteRdbfBlm4TrinityLmC.bin \n")
-ssh.stdin.write("chmod 777  /rcs/bbmContLteRdbfBlm4TrinityLmC.bin \n")
-ssh.stdin.write("reboot \n")
-stdout, stderr = ssh.communicate()
-print(stdout)
-ssh.stdin.close()
+    host="root@"+sys.argv[1]
+    ssh = subprocess.Popen(["ssh", "%s" % host],
+        shell=False,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True,
+        bufsize=0)
+    ssh.stdin.write("ln -s /rcs/bbmContLteRdbfBlm4TrinityLmC.bin /home/sirpa/dev_patches/bbmContLteRdbfBlm4TrinityLmC.bin \n")
+    ssh.stdin.write("chmod 777  /home/sirpa/dev_patches/bbmContLteRdbfBlm4TrinityLmC.bin \n")
+    ssh.stdin.write("chmod 777  /rcs/bbmContLteRdbfBlm4TrinityLmC.bin \n")
+    ssh.stdin.write("reboot \n")
+    stdout, stderr = ssh.communicate()
+    print(stdout)
+    ssh.stdin.close()
 
