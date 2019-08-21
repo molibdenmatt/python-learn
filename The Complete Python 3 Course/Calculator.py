d4ea947ce75435135af263777de9deecced41cb7
Mateusz Świętoń <mateuszswieton@MacBook-Pro-Mateusz.local>
2019-08-21 21:42:41 +0200

    home update

---
+++ The Complete Python 3 Course/Calculator.py
@@ -0,0 +1,33 @@
+import re
+# Eval is dangerous! 
+
+print("Our magical calculator")
+print("type quit to exit")
+
+
+previous = 0
+run = True
+
+def performMath():
+    global run
+    global previous
+    equation = ""
+    if previous == 0:
+        equation = input("Enter equation:")
+    else:
+        equation = input(str(previous))
+
+
+    if equation == 'quit':
+        print('Bye Human!')
+        run = False
+    else:
+        equation = re.sub('[a-zA-Z,.:' ']', '', equation)
+        if previous == 0:
+            previous = eval(equation)
+        else:
+            previous = eval(str(previous) + equation)
+
+
+while run:
+    performMath()
