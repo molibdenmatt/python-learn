449e394615990a71b331771870614786e6ebc3c1
molibdentmatt <mateusz.swieton@gmail.com>
2019-08-21 13:35:52 +0200

    pre RPG project commit

---
+++ The Complete Python 3 Course/Guidlines.py
@@ -0,0 +1,24 @@
+import random
+import sys
+
+# Multiple libs should be on different lines
+# from MyModule include foo, bar, foobar
+# But multiple modules from same library can be comma-separated
+
+# Two empty lines between functions
+
+
+def my_function(one, two,
+                three, four,
+                five, six):
+    print("Hello World")
+
+
+def second_function():
+    print("Second function")
+
+my_list = [1, 2,
+           3, 4,
+           5, 6]
+
+
