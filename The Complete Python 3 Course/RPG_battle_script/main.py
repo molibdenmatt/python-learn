9a520a84f36b2d371691950bdf21d13b879934ea
molibdentmatt <mateusz.swieton@gmail.com>
2019-08-26 13:22:43 +0200

    28_08 update

--- The Complete Python 3 Course/RPG_battle_script/main.py
+++ The Complete Python 3 Course/RPG_battle_script/main.py
@@ -1,3 +1,26 @@
-from classes.game import Person, bcolors
+from RPG_battle_script.classes.game import bcolors, Person
 
-player = Person(60
+
+magic = [{"name": "Fire", "cost": 10, "dmg": 60},
+         {"name": "Thunder", "cost": 12, "dmg": 80},
+         {"name": "Blizzard", "cost": 10, "dmg": 60}]
+
+player = Person(460, 65, 60, 34, magic)
+enemy = Person(1200, 65, 45, 25, magic)
+
+running = True
+i = 0
+
+print(bcolors.FAIL + bcolors.BOLD + "AN ENEMY ATTACKS!" + bcolors.ENDC)
+
+while running:
+    print("==========================")
+    player.chose_action()
+    choice = input("Choose your action:")
+    index = int(choice) - 1
+
+    if index == 0:
+            dmg = player.generate_damage()
+            enemy.take_damage(dmg)
+            print("You attacked for", dmg, "points. Enemy HP:", enemy.get_hp())
+
---
+++ The Complete Python 3 Course/classes/__init__.py
@@ -0,0 +1,2 @@
+def game():
+    return None
