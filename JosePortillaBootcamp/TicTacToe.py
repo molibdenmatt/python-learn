c8ac0ecb209a193fdfae166e4480ff5ef84d4dcd
mateusz.swieton@nokia.com <mateusz.swieton@nokia.com>
2021-02-04 18:06:42 +0100

    Add initial folder and playgronud python script

---
+++ JosePortillaBootcamp/TicTacToe.py
@@ -0,0 +1,13 @@
+def display(row1, row2, row3):
+    print(row1)
+    print(row2)
+    print(row3)
+
+
+row1 = [' ', ' ', ' ']
+row2 = [' ', ' ', ' ']
+row3 = [' ', ' ', ' ']
+
+display(row1, row2, row3)
+
+response = input('Please enter a value: ')
