0347ee3df576c454db0a4a0be660f8662239e4c4
Mateusz Swieton <mateusz.swieton@gmail.com>
2021-02-04 19:47:51 +0100

    Update on files 4.02

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -2,22 +2,31 @@ import sys
 import pygame
 
 
+def check_keydown_events(event, ship):
+    """Reacts to pressing a key"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    """Reacts to key release"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
 def check_events(ship):
-    """Ractions to user input - keyboard&mouse"""
+    """Reactions to user input - keyboard&mouse"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
-
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
 
 
 def update_screen(ai_settings, screen, ship):
